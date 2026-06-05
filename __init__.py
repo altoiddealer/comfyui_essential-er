59d1139c472f2a71bb32743a9477e3e0f76d3c64
altoiddealer <altoiddealer@gmail.com>
2026-06-05 12:01:46 -0400

    New node, MergeImageBatchList

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
-from .image import SmartImageResizeAlt
+from .image import SmartImageResizeAlt, MergeImageBatchList
 
-NODE_CLASS_MAPPINGS = { "SmartImageResizeAlt": SmartImageResizeAlt }
+NODE_CLASS_MAPPINGS = {"SmartImageResizeAlt": SmartImageResizeAlt,
+                       "MergeImageBatchList": MergeImageBatchList,
+                       }
 
-NODE_DISPLAY_NAME_MAPPINGS = { "SmartImageResizeAlt": "🔧 Smart Image Resize Alt ◯" }
+NODE_DISPLAY_NAME_MAPPINGS = {"SmartImageResizeAlt": "🔧 Smart Image Resize Alt ◯",
+                              "MergeImageBatchList": "🔧 MergeImageBatchList ◯",
+                              }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

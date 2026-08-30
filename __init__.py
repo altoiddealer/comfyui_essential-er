44d970cfe43c9047c084a7b89f97510d22e03f7b
altoiddealer <altoiddealer@gmail.com>
2026-08-30 17:11:26 -0400

    New node: Pass or None

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from comfy_api.latest import ComfyExtension
 from .nodes_image import ResizeImageMaskAlt, SmartImageResizeAlt
 from .nodes_videos import LoadVideosFromFolderList
 from .nodes_batching import MergeImageBatchList, MergeImageBatchAndAudioList
-
+from .nodes_utility import PassOrNone
 
 class MyExtension(ComfyExtension):
     async def get_node_list(self) -> list[type]:
@@ -13,6 +13,7 @@ class MyExtension(ComfyExtension):
             LoadVideosFromFolderList,
             MergeImageBatchList,
             MergeImageBatchAndAudioList,
+            PassOrNone,
         ]
 
 
